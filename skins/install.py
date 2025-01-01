3236ad7d9403324c1c40ef510c62d721521d6170
Shunra <43567521+swfrx@users.noreply.github.com>
2025-01-01 13:58:29 +0000

    Amended to create the list of skins using a Python routine

---
+++ skins/install.py
@@ -0,0 +1,43 @@
+# installer for the 'Index' skin
+# January 2025, Sally Woolrich
+
+from weecfg.extension import ExtensionInstaller
+
+#-------- extension info -----------
+
+VERSION      = "1.0"
+NAME         = 'Index'
+DESCRIPTION  = 'Generates a basic index.html for my live & test systems based on directoris in HTML_ROOT containing index.html. At present DokuWiki is an exception.'
+AUTHOR       = "Sally Woolrich"
+AUTHOR_EMAIL = "shunracats <at> gmail.com"
+
+#-------- main loader -----------
+
+def loader():
+    return IndexInstaller()
+
+class IndexInstaller(ExtensionInstaller):
+    def __init__(self):
+        super(IndexInstaller, self).__init__(
+            version=VERSION,
+            name=NAME,
+            description=DESCRIPTION,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            config={
+                'StdReport': {
+                    'Index': {
+                        'skin': 'Index',
+                        'enable' : 'True',
+                        'lang': 'en',
+                    },
+                },
+            },
+            files=[('bin/user', ['bin/user/index.py']),
+                ('skins/Index',
+                 ['skins/Index/lang/en.conf',
+                  'skins/Index/dokuwiki/index.html.tmpl',
+                  'skins/Index/skin.conf',
+                  ]),
+            ]
+        )
