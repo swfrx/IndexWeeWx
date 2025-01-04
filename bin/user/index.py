f75926bd5bfae8ff29ec0635e86f380f5f5630b0
Shunra <43567521+swfrx@users.noreply.github.com>
2025-01-04 09:21:21 +0000

    Update index.py
    Handling of files / directories in HTML_ROOT amended.
    
    Now uses 'standard' code to get HTML_ROOT

--- bin/user/index.py
+++ bin/user/index.py
@@ -25,7 +25,8 @@ def logerr(msg):
 from weewx.cheetahgenerator import SearchList
 
 # Print version in syslog for easier troubleshooting
-VERSION = "2.0"
+VERSION = "2.3"
+
 loginf("index.py Version %s" % VERSION)
 
 class getData(SearchList):
@@ -37,17 +38,28 @@ class getData(SearchList):
         Return a string that contains a table with links
         to each index.html or sguweewx.html in HTML_ROOT
         """
-        index_debug = self.generator.skin_dict["Extras"].get( "index_debug", 0 )
+        index_debug = int(self.generator.skin_dict["Extras"].get( "index_debug", 0 ))
         if (index_debug):
-            logdbg( "index_debug = %s" % index_debug )
+            logerr( "index_debug = %s" % index_debug )
 
-        try:
-            html_root = self.skin_dict['HTML_ROOT']
-        except:
-            html_root = "/var/www/html/weewx"
+        skin_version = self.generator.skin_dict['SKIN_VERSION']
+        if( index_debug) :
+            logerr( "SKIN_VERSION %s" % skin_version )
+
+# Find the right HTML ROOT
+        if "HTML_ROOT" in self.generator.skin_dict:
+            html_root = os.path.join(
+                self.generator.config_dict["WEEWX_ROOT"],
+                self.generator.skin_dict["HTML_ROOT"],
+            )
+        else:
+            html_root = os.path.join(
+                self.generator.config_dict["WEEWX_ROOT"],
+                self.generator.config_dict["StdReport"]["HTML_ROOT"],
+            )
 
         if (index_debug):
-            logdbg( "HTML_ROOT = %s" % html_root )
+            logerr( "HTML_ROOT = %s" % html_root )
 
         os.chdir( html_root )
 
@@ -57,16 +69,21 @@ class getData(SearchList):
         items =  os.listdir(html_root)
         items.sort(key=str.lower)
         for dirname in items:
-            logdbg( "dirname = %s" % dirname )
+            if (index_debug):
+                logerr( "dirname = %s" % dirname )
             if (dirname == "dokuwiki" ):
                 skin_list = skin_list + os.linesep + "<h4><a href='dokuwiki/sguweewx.html'>SGC Weather Station</a></h4>"
             else:
+                displayname = dirname
+                if (dirname != "NewNeoWx" ):
+                    displayname = displayname.capitalize()
                 link = os.path.join(dirname, "index.html" )
                 if os.path.isfile( link ):
-                    if ( dirname != "NewNeoWx" ):
-                        dirname = dirname.capitalize()
-                    skin_list = skin_list + os.linesep + "<h4><a href='" + dirname + "'>" + dirname + "</a></h4>"
+                    skin_list = skin_list + os.linesep + "<h4><a href='" + dirname + "'>" + displayname + "</a></h4>"
 
+
+        if (index_debug):
+            logerr( "skin_list = %s" % skin_list )
         # Finally, return our extension
         search_list_extension = { 'skin_list' : skin_list }
         return [search_list_extension]
